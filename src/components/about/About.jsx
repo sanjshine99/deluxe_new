4f45d8c2c90c2d70c94907e4e0e37c273320c7c6
Sanjana Bandara <sanjana.bandara@cube.global>
2026-08-04 09:41:40 +0530

    fix(): css issue

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import "../home/Home.css";
-import "../home/homecomponents/HomeComponent.css";
 import "./About.css";
 import GoToTop from "../functions/GoToTop";
 
